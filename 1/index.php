fe43682863316951a8faa2f8c75d98b3f964c125
agostinoluca <agostino.luca89@gmail.com>
2024-03-01 19:37:33 +0100

    Initial commit

---
+++ 1/index.php
@@ -0,0 +1,40 @@
+<?php
+/*
+Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario. 
+Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. 
+Stampiamo a schermo tutte le partite con questo schema:
+Olimpia Milano - Cantù | 55-60
+*/
+
+$matchBasket = [
+    ["home" => "Olimpia Milano", "out" => "Cantù", "pointsHome" => rand(1, 100), "pointsOut" => rand(1, 100)],
+    ["home" => "Genova", "out" => "Bari", "pointsHome" => rand(1, 100), "pointsOut" => rand(1, 100)],
+    ["home" => "Torino", "out" => "Lecce", "pointsHome" => rand(1, 100), "pointsOut" => rand(1, 100)]
+];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Snaks</title>
+</head>
+
+<body>
+    <h5>
+        <?php
+        foreach ($matchBasket as $match) { ?> // oppure utilizzare :
+
+            <span>
+                <?php
+                echo $match["home"] . " - " . $match["out"] . " | " . $match["pointsHome"] . "-" . $match["pointsOut"] . "<br>";
+                ?>
+            </span>
+
+        <?php }; ?> // endforeach;
+    </h5>
+</body>
+
+</html>
---
+++ 2/index.php
@@ -0,0 +1,24 @@
+<?php /*// Creare un array con 15 numeri casuali, tenendo conto che l’array non dovrà contenere lo stesso numero più di una volta */
+$randomNumbers = [];
+$min = 1;
+$max = 100;
+
+
+
+function randomNumberGenerate($min, $max, $randomNumbers)
+{
+    $number = rand($min, $max);
+    while (in_array($number, $randomNumbers)) {
+        $number = rand($min, $max);
+    };
+    return $number;
+};
+
+while (count($randomNumbers) < 15) {
+    $randomNumber = randomNumberGenerate($min, $max, $randomNumbers);
+    $randomNumbers[] = $randomNumber;
+};
+
+foreach ($randomNumbers as $number) {
+    echo $number . "<br>";
+};
